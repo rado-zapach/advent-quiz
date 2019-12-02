c20ba049e6de1ea5cb76766ad07f6e240ba8434a
rado-zapach <r.zapach@gmail.com>
2019-12-02 20:41:41 +0100

    Fix answer modal opening even after user auth expires

--- src/main/webapp/app/blocks/interceptor/auth-expired.interceptor.ts
+++ src/main/webapp/app/blocks/interceptor/auth-expired.interceptor.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 import { LoginService } from 'app/core/login/login.service';
-import { LoginModalService } from 'app/core/login/login-modal.service';
 import { StateStorageService } from 'app/core/auth/state-storage.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable()
 export class AuthExpiredInterceptor implements HttpInterceptor {
   constructor(
     private loginService: LoginService,
-    private loginModalService: LoginModalService,
     private stateStorageService: StateStorageService,
-    private router: Router
+    private router: Router,
+    private modalService: NgbModal
   ) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -25,7 +25,7 @@ export class AuthExpiredInterceptor implements HttpInterceptor {
             this.stateStorageService.storeUrl(this.router.routerState.snapshot.url);
             this.loginService.logout();
             this.router.navigate(['']);
-            this.loginModalService.open();
+            this.modalService.dismissAll();
           }
         }
       })
