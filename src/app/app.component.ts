b8a1177229bb4ad7fea69015cd392a920a954781
Radoslav Zapach <radoslav.zapach@iblsoft.com>
2023-11-14 19:05:52 +0100

    add google oauth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {RouterOutlet} from '@angular/router';
+import {Auth, CognitoHostedUIIdentityProvider} from "@aws-amplify/auth";
 import {AmplifyAuthenticatorModule} from "@aws-amplify/ui-angular";
+import {CognitoUser} from "amazon-cognito-identity-js";
+import {catchError, from, map, of} from "rxjs";
 import {ZenObservable} from "zen-observable-ts";
 import {APIService, Todo} from "./API.service";
 
@@ -18,6 +21,8 @@ export class AppComponent {
   public todos: Array<Todo> = [];
   private subscription: ZenObservable.Subscription | null = null;
 
+  public user$ = from(Auth.currentAuthenticatedUser()).pipe(map((user: CognitoUser) => user.getUsername()), catchError(err => of(console.log(err))));
+
   constructor(private api: APIService, private fb: FormBuilder) {
     this.createForm = this.fb.group({
       name: ['', Validators.required],
@@ -30,7 +35,6 @@ export class AppComponent {
       this.todos = event.items as Todo[];
     });
 
-    /* subscribe to new restaurants being created */
     this.subscription = this.api.OnCreateTodoListener().subscribe(
       (event: any) => {
         const newTodo = event.value.data.onCreateTodo;
@@ -49,6 +53,15 @@ export class AppComponent {
       .catch((e) => {
         console.log('error creating item...', e);
       });
+  }
+
+  public openGoogleAuth(): void {
+    console.log(1);
+    Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google });
+  }
+
+  public signOut(): void {
+    Auth.signOut();
   }
 
   ngOnDestroy() {
