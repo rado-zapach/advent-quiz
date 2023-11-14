01f8e36e5d739b21e4ccf7718142f341aaa3ea70
Radoslav Zapach <radoslav.zapach@iblsoft.com>
2023-11-14 20:04:12 +0100

    add dev and prod auth redirect urls

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,41 @@ import {bootstrapApplication} from '@angular/platform-browser';
 import {Amplify} from 'aws-amplify';
 import {AppComponent} from './app/app.component';
 import {appConfig} from './app/app.config';
-import aws_exports from './aws-exports';
+import awsConfig from './aws-exports';
 
-Amplify.configure(aws_exports);
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === "[::1]" ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+);
+
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [
+  localRedirectSignIn,
+  devRedirectSignIn,
+  prodRedirectSignIn,
+] = awsConfig.oauth.redirectSignIn.split(",");
+
+const [
+  localRedirectSignOut,
+  devRedirectSignOut,
+  prodRedirectSignOut,
+] = awsConfig.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = {
+  ...awsConfig,
+  oauth: {
+    ...awsConfig.oauth,
+    redirectSignIn: isLocalhost ? localRedirectSignIn : devRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : prodRedirectSignOut,
+  }
+}
+
+Amplify.configure(updatedAwsConfig);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
