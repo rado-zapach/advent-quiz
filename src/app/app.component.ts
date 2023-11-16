2979160756ac8abcbfc0813ce4ce058f716a615e
Radoslav Zapach <radoslav.zapach@iblsoft.com>
2023-11-16 20:47:54 +0100

    change to npm and amplify 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,21 @@
 import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {RouterOutlet} from '@angular/router';
-import {Auth, CognitoHostedUIIdentityProvider} from "@aws-amplify/auth";
-import {AmplifyAuthenticatorModule} from "@aws-amplify/ui-angular";
-import {CognitoUser} from "amazon-cognito-identity-js";
-import {catchError, from, map, of} from "rxjs";
-import {ZenObservable} from "zen-observable-ts";
-import {APIService, Todo} from "./API.service";
+import {AmplifyAuthenticatorModule} from '@aws-amplify/ui-angular';
+import {TodosComponent} from './todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AmplifyAuthenticatorModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TodosComponent,
+    AmplifyAuthenticatorModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public createForm: FormGroup;
-  public todos: Array<Todo> = [];
-  private subscription: ZenObservable.Subscription | null = null;
-
-  public user$ = from(Auth.currentAuthenticatedUser()).pipe(map((user: CognitoUser) => user.getUsername()), catchError(err => of(console.log(err))));
-
-  constructor(private api: APIService, private fb: FormBuilder) {
-    this.createForm = this.fb.group({
-      name: ['', Validators.required],
-      description: ['', Validators.required],
-    });
-  }
-
-  async ngOnInit() {
-    this.api.ListTodos().then(event => {
-      this.todos = event.items as Todo[];
-    });
-
-    this.subscription = this.api.OnCreateTodoListener().subscribe(
-      (event: any) => {
-        const newTodo = event.value.data.onCreateTodo;
-        this.todos = [newTodo, ...this.todos];
-      }
-    );
-  }
-
-  public onCreate(todo: Todo) {
-    this.api
-      .CreateTodo(todo)
-      .then(() => {
-        console.log('item created!');
-        this.createForm.reset();
-      })
-      .catch((e) => {
-        console.log('error creating item...', e);
-      });
-  }
-
-  public openGoogleAuth(): void {
-    console.log(1);
-    Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google });
-  }
-
-  public signOut(): void {
-    Auth.signOut();
-  }
-
-  ngOnDestroy() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-    this.subscription = null;
-  }
+  title = 'amplifyapp';
 }
---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,67 @@
+import {Component, OnInit} from '@angular/core';
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {generateClient} from 'aws-amplify/api';
+import * as mutations from '../../graphql/mutations';
+import * as queries from '../../graphql/queries';
+import * as subscriptions from '../../graphql/subscriptions';
+import {ListTodosQuery, Todo} from '../API.service';
+
+@Component({
+  standalone: true,
+  selector: 'app-todos',
+  imports: [ReactiveFormsModule],
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  // @ts-ignore
+  public todos: ListTodosQuery['listTodos']['items'] = [];
+  public createForm: FormGroup;
+  public client = generateClient();
+
+  constructor(private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+
+    this.client
+      .graphql({
+        query: subscriptions.onCreateTodo
+      }).pipe(takeUntilDestroyed())
+      .subscribe({
+        next: (event) => {
+          const newTodo: Todo = event.data.onCreateTodo;
+          this.todos = [newTodo, ...this.todos];
+        }
+      });
+  }
+
+  async ngOnInit() {
+    /* fetch todos when app loads */
+    try {
+      const response = await this.client.graphql({
+        query: queries.listTodos
+      });
+      this.todos = response.data.listTodos.items;
+    } catch (e) {
+      console.log('error fetching todos', e);
+    }
+  }
+
+  public async onCreate(todo: Todo) {
+    try {
+      const response = await this.client.graphql({
+        query: mutations.createTodo,
+        variables: {
+          input: todo
+        }
+      });
+      console.log('item created!', response);
+      this.createForm.reset();
+    } catch (e) {
+      console.log('error creating todo...', e);
+    }
+  }
+}
