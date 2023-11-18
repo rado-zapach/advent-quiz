b27481550ecc58623dc837422c946aaaa062f8a9
Radoslav Zapach <radoslav.zapach@iblsoft.com>
2023-11-18 19:38:43 +0100

    work on calendar component

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,27 +1,51 @@
-import {CommonModule} from '@angular/common';
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {ChangeDetectionStrategy, Component, inject, OnInit, signal} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {generateClient} from 'aws-amplify/api';
 import * as queries from '../../graphql/queries';
+import {PlayerQuestion} from '../API.service';
+import {QuestionComponent} from '../question/question.component';
+
+interface Question extends PlayerQuestion {
+    day: number;
+}
 
 @Component({
     selector: 'app-calendar',
     standalone: true,
-    imports: [CommonModule, MatButtonModule],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        NgOptimizedImage,
+        MatTooltipModule,
+        MatDialogModule,
+        MatProgressBarModule,
+    ],
     templateUrl: './calendar.component.html',
     styleUrl: './calendar.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
+    public dialog = inject(MatDialog);
     public readonly client = generateClient();
+    public questions = signal<Question[]>([]);
 
-    public async getQuestions(): Promise<void> {
-        // const response = await this.client.graphql({
-        //     query: queries.listQuestions,
-        // });
+    public async ngOnInit(): Promise<void> {
         const response = await this.client.graphql({
             query: queries.playerQuestionList,
         });
-        console.log(response);
+        const questions = response.data.playerQuestionList.map(q => ({
+            ...q,
+            day: new Date(q.openTime).getDate(),
+        }));
+        questions.sort((a, b) => new Date(a.openTime).getTime() - new Date(b.openTime).getTime());
+        this.questions.set(questions);
+    }
+
+    public openQuestion(id: string): void {
+        this.dialog.open(QuestionComponent);
     }
 }
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,10 +1,12 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
     selector: 'app-question',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatDialogModule, MatButtonModule],
     templateUrl: './question.component.html',
     styleUrl: './question.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
