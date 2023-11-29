f686004c03c66d5430c854691882c00027c59f7b
Radoslav Zapach <radoslav.zapach@iblsoft.com>
2023-11-29 09:22:40 +0100

    use window.location.origin as login redirect

--- src/main.ts
+++ src/main.ts
@@ -14,27 +14,12 @@ if (myParam) {
     document.body.appendChild(elemDiv);
 }
 
-const isLocalhost = Boolean(
-    window.location.hostname === 'localhost' ||
-        // [::1] is the IPv6 localhost address.
-        window.location.hostname === '[::1]' ||
-        // 127.0.0.1/8 is considered localhost for IPv4.
-        window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-);
-
-// Assuming you have two redirect URIs, and the first is for localhost and second is for production
-const [localRedirectSignIn, devRedirectSignIn, prodRedirectSignIn] =
-    awsConfig.oauth.redirectSignIn.split(',');
-
-const [localRedirectSignOut, devRedirectSignOut, prodRedirectSignOut] =
-    awsConfig.oauth.redirectSignOut.split(',');
-
 const updatedAwsConfig = {
     ...awsConfig,
     oauth: {
         ...awsConfig.oauth,
-        redirectSignIn: isLocalhost ? localRedirectSignIn : devRedirectSignIn,
-        redirectSignOut: isLocalhost ? localRedirectSignOut : devRedirectSignOut,
+        redirectSignIn: `${window.location.origin}/`,
+        redirectSignOut: `${window.location.origin}/`,
     },
 };
 Amplify.configure(updatedAwsConfig);
