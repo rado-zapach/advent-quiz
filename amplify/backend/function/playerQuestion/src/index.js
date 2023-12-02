791ceeffbbc138adbcecb057b2ebe6d7198fe144
Radoslav Zapach <radoslav.zapach@iblsoft.com>
2023-12-02 13:04:49 +0100

    make playerQuestion use dynamoDB directly

--- amplify/backend/function/playerQuestion/src/index.js
+++ amplify/backend/function/playerQuestion/src/index.js
@@ -1,59 +1,22 @@
-import crypto from "@aws-crypto/sha256-js";
-import {defaultProvider} from "@aws-sdk/credential-provider-node";
-import {SignatureV4} from "@aws-sdk/signature-v4";
-import {HttpRequest} from "@aws-sdk/protocol-http";
-import {default as fetch, Request} from "node-fetch";
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import {DynamoDBDocumentClient, GetCommand} from "@aws-sdk/lib-dynamodb";
 
-const {Sha256} = crypto;
-const GRAPHQL_ENDPOINT = process.env.API_ADVENTQUIZ_GRAPHQLAPIENDPOINTOUTPUT;
-const AWS_REGION = process.env.AWS_REGION || "us-east-1";
-
-const findQuery = questionId => /* GraphQL */ `
-  query MyQuery {
-    getQuestion(id: "${questionId}") {
-      id
-      text
-      choices
-      icon
-      correctAnswer
-      openTime
-      closeTime
-      winner
-    }
-  }
-`;
-
-const endpoint = new URL(GRAPHQL_ENDPOINT);
-const signer = new SignatureV4({
-    credentials: defaultProvider(),
-    region: AWS_REGION,
-    service: "appsync",
-    sha256: Sha256,
-});
-
-async function MakeRequest(query) {
-    const requestToBeSigned = new HttpRequest({
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            host: endpoint.host,
-        },
-        hostname: endpoint.host,
-        body: JSON.stringify({query}),
-        path: endpoint.pathname,
-    });
-    const signed = await signer.sign(requestToBeSigned);
-    const request = new Request(GRAPHQL_ENDPOINT, signed);
-    const response = await fetch(request);
-    return await response.json();
-}
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async event => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
 
     const questionId = event.arguments.questionId;
-    const question = await MakeRequest(findQuery(questionId));
-    const i = question.data.getQuestion;
+
+    const command = new GetCommand({
+        TableName: `Question-${process.env.API_ADVENTQUIZ_GRAPHQLAPIIDOUTPUT}-${process.env.ENV}`,
+        Key: {
+            id: questionId,
+        },
+    });
+    const response = await docClient.send(command);
+    const i = response.Item;
 
     const now = new Date().getTime();
     const openTime = new Date(i.openTime).getTime();
