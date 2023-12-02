2daaed3ba463d121b1de50a85aef6c6b88c25e0e
Radoslav Zapach <radoslav.zapach@iblsoft.com>
2023-12-02 17:19:52 +0100

    make playerAnswerList use dynamoDB directly

--- amplify/backend/function/playerAnswerList/src/index.js
+++ amplify/backend/function/playerAnswerList/src/index.js
@@ -1,57 +1,34 @@
-import {Sha256} from "@aws-crypto/sha256-js";
-import {defaultProvider} from "@aws-sdk/credential-provider-node";
-import {SignatureV4} from "@aws-sdk/signature-v4";
-import {HttpRequest} from "@aws-sdk/protocol-http";
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import {DynamoDBDocumentClient, GetCommand, ScanCommand} from "@aws-sdk/lib-dynamodb";
 
-const GRAPHQL_ENDPOINT = process.env.API_ADVENTQUIZ_GRAPHQLAPIENDPOINTOUTPUT;
-const AWS_REGION = process.env.AWS_REGION || "us-east-1";
-
-const endpoint = new URL(GRAPHQL_ENDPOINT);
-const signer = new SignatureV4({
-    credentials: defaultProvider(),
-    region: AWS_REGION,
-    service: "appsync",
-    sha256: Sha256,
-});
-
-async function MakeRequest(query) {
-    const requestToBeSigned = new HttpRequest({
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            host: endpoint.host,
-        },
-        hostname: endpoint.host,
-        body: JSON.stringify({query}),
-        path: endpoint.pathname,
-    });
-    const signed = await signer.sign(requestToBeSigned);
-    const request = new Request(GRAPHQL_ENDPOINT, signed);
-    const response = await fetch(request);
-    return await response.json();
-}
-
-const findQuery = questionId => /* GraphQL */ `
-  query MyQuery {
-    listAnswers(filter: {questionId: {eq: "${questionId}"}}) {
-      items {
-        id
-        player
-      }
-    }
-    getQuestion(id: "${questionId}") {
-      closeTime
-    }
-  }
-`;
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async event => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
 
     const questionId = event.arguments.questionId;
-    const answersAndQuestion = await MakeRequest(findQuery(questionId));
-    const answers = answersAndQuestion.data.listAnswers.items;
-    const question = answersAndQuestion.data.getQuestion;
+
+    const questionCommand = new GetCommand({
+        TableName: `Question-${process.env.API_ADVENTQUIZ_GRAPHQLAPIIDOUTPUT}-${process.env.ENV}`,
+        Key: {
+            id: questionId,
+        },
+    });
+    const fetchQuestion = docClient.send(questionCommand);
+
+    const answersCommand = new ScanCommand({
+        TableName: `Answer-${process.env.API_ADVENTQUIZ_GRAPHQLAPIIDOUTPUT}-${process.env.ENV}`,
+        FilterExpression: "questionId = :questionId",
+        ExpressionAttributeValues: {
+            ":questionId": questionId,
+        },
+    });
+    const fetchAnswers = await docClient.send(answersCommand);
+
+    const [questionResponse, answersResponse] = await Promise.all([fetchQuestion, fetchAnswers]);
+    const question = questionResponse.Item;
+    const answers = answersResponse.Items;
 
     const now = new Date().getTime();
     const closeTime = new Date(question.closeTime).getTime();
@@ -60,5 +37,8 @@ export const handler = async event => {
     if (!isClosed) {
         throw new Error("Question not closed yet!");
     }
-    return answers;
+    return answers.map(a => ({
+        id: a.id,
+        player: a.player,
+    }));
 };
